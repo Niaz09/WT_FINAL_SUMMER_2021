46f57bf318eb343fcdbd74b8e6448350b85f946e
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-07-19 10:50:21 +0600

    Lab Task 3 v1

---
+++ Lab Task 3/admin_header.php
@@ -0,0 +1,7 @@
+<html>
+  <body>
+      <h1>Admin</h1>
+      <td><a href="dashboard.php">Dashboard</a></td>
+      <td><a href="allstudents.php">All Students</a></td>
+      <td><a href="addstudent.php">Add Student</a></td>
+      <td><a href="login.php">Logout</a></td>
